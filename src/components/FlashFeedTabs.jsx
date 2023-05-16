652f68c6bedcbdbe9f573e10c586ca98722e4060
Amber Jain <jainanuamber@gmail.com>
2023-05-16 15:57:26 +0530

    custom flashfeed tabs created

--- src/components/FlashFeedTabs.jsx
+++ src/components/FlashFeedTabs.jsx
@@ -2,7 +2,8 @@ import { StyleSheet, useWindowDimensions } from "react-native";
 import { useState } from "react";
 import { SceneMap, TabView } from "react-native-tab-view";
 import DiscoverScreen from "../screens/DiscoverScreen";
-import  NewsScreen  from "../screens/NewsScreen";
+import NewsScreen from "../screens/NewsScreen";
+import TopNavigation from "./TopNavigation";
 
 const FlashFeedTabs = () => {
   const layout = useWindowDimensions();
@@ -21,6 +22,7 @@ const FlashFeedTabs = () => {
       renderScene={renderScene}
       onIndexChange={setIndex}
       initialLayout={{ width: layout.width }}
+      renderTabBar={() => <TopNavigation index={index} setIndex={setIndex} />}
     />
   );
 };
