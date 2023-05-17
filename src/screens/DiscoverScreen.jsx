fb0b9b844c90be34648fc08818ce6838d30c49e2
Amber Jain <jainanuamber@gmail.com>
2023-05-17 06:28:39 +0530

    completed category section

--- src/screens/DiscoverScreen.jsx
+++ src/screens/DiscoverScreen.jsx
@@ -1,12 +1,74 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from "react-native";
+import React, { useContext } from "react";
+import { NewsContext } from "../api/Context";
+import { categories, sources } from "../api/Api";
+import Carousel from "react-native-snap-carousel";
+
+const windowWidth = Dimensions.get("window").width;
+const SLIDE_WIDTH = Math.round(windowWidth / 3.5);
 
 const DiscoverScreen = () => {
+  const { setCategory } = useContext(NewsContext);
   return (
-    <View>
-      <Text>DiscoverScreen</Text>
+    <View style={styles.discoverContainer}>
+      {/* search */}
+      {/* categories */}
+      <Text style={{ ...styles.subtitle, color: "#ffffff" }}>Categories</Text>
+      <Carousel
+        layout='default'
+        data={categories}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            style={styles.category}
+            onPress={() => setCategory(item.name)}>
+            <Image
+              source={{ uri: item.pic }}
+              style={styles.categoryImage}
+              resizeMode='contain'
+            />
+            <Text style={{ ...styles.name, color: "#ffffff" }}>
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+        sliderWidth={windowWidth}
+        itemWidth={SLIDE_WIDTH}
+        activeSlideAlignment='start'
+        inactiveSlideScale={1}
+        inactiveSlideOpacity={0.5}
+      />
+      {/* sources */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  discoverContainer: { padding: 10, alignItems: "center" },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingBottom: 8,
+    marginHorizontal: 5,
+    borderBottomColor: "#007FFF",
+    borderBottomWidth: 5,
+    alignSelf: "flex-start",
+    borderRadius: 10,
+  },
+  categoryImage: { height: "60%", width: "100%" },
+  name: { fontSize: 14, textTransform: "capitalize" },
+  category: {
+    height: 130,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+});
 
 export default DiscoverScreen;
